9c704148e790fdafd6f17ab96c9f752036f0fc28
graciagarpin <graciagarpin@gmail.com>
2022-04-26 09:23:02 +0200

    html basic structure

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,36 @@ function App() {
   return (
     <div>
       <h1>Frases de Friends</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              Filtrar por frase
+              <input type="text" />
+            </th>
+            <th>
+              Filtrar por personaje
+              <input type="text" />
+            </th>
+          </tr>
+        </thead>
+        <tbody id="cuerpoTabla">
+          <td>
+            <tr>hola</tr>
+            <tr>hola</tr>
+            <tr>hola</tr>
+            <tr>hola</tr>
+            <tr>hola</tr>
+          </td>
+          <td>
+            <tr>hola</tr>
+            <tr>hola</tr>
+            <tr>hola</tr>
+            <tr>hola</tr>
+            <tr>hola</tr>
+          </td>
+        </tbody>
+      </table>
     </div>
   );
 }
